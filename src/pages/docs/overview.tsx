bf0556199b9a281f258a92dbd5c663ceaa09fd57
ParsaKhaz <khazapar7@gmail.com>
2024-11-26 16:20:36 -0800

    bugs

--- src/pages/docs/overview.tsx
+++ src/pages/docs/overview.tsx
@@ -1,11 +1,20 @@
-import { Overview } from "@/components/docs/Overview";
 import DocsLayout from "@/components/DocsLayout";
 import { DocsSidebar } from "@/components/DocsSidebar";
+import { Overview } from "@/components/docs/Overview";
+import { useState } from "react";
 
 export default function OverviewPage() {
+  const [isTitleUnlocked, setIsTitleUnlocked] = useState(false);
+
   return (
     <DocsLayout
-      sidebar={<DocsSidebar currentPath="/docs/overview" />}
+      sidebar={
+        <DocsSidebar 
+          currentPath="/docs/overview" 
+          onTitleClick={() => {}}
+          isTitleUnlocked={isTitleUnlocked}
+        />
+      }
       main={<Overview />}
     />
   );
--- src/pages/docs/usage.tsx
+++ src/pages/docs/usage.tsx
@@ -1,11 +1,20 @@
-import { Usage } from "@/components/docs/Usage";
 import DocsLayout from "@/components/DocsLayout";
 import { DocsSidebar } from "@/components/DocsSidebar";
+import { Usage } from "@/components/docs/Usage";
+import { useState } from "react";
 
 export default function UsagePage() {
+  const [isTitleUnlocked, setIsTitleUnlocked] = useState(false);
+
   return (
     <DocsLayout
-      sidebar={<DocsSidebar currentPath="/docs/usage" />}
+      sidebar={
+        <DocsSidebar 
+          currentPath="/docs/usage" 
+          onTitleClick={() => {}}
+          isTitleUnlocked={isTitleUnlocked}
+        />
+      }
       main={<Usage />}
     />
   );
