27269be13ffbe9daf725685fc33536e915be8803
ParsaKhaz <khazapar7@gmail.com>
2024-11-26 16:18:37 -0800

    bugs

--- src/pages/docs/installation.tsx
+++ src/pages/docs/installation.tsx
@@ -1,11 +1,20 @@
-import { Installation } from "@/components/docs/Installation";
 import DocsLayout from "@/components/DocsLayout";
 import { DocsSidebar } from "@/components/DocsSidebar";
+import { Installation } from "@/components/docs/Installation";
+import { useState } from "react";
 
 export default function InstallationPage() {
+  const [isTitleUnlocked, setIsTitleUnlocked] = useState(false);
+
   return (
     <DocsLayout
-      sidebar={<DocsSidebar currentPath="/docs/installation" />}
+      sidebar={
+        <DocsSidebar 
+          currentPath="/docs/installation" 
+          onTitleClick={() => {}} // No-op since we don't need title editing here
+          isTitleUnlocked={isTitleUnlocked}
+        />
+      }
       main={<Installation />}
     />
   );
